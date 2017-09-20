1209fb15e22a416b62d83c61acdf8e0f6be2e517
alethiophile <>
2017-09-19 18:19:53 -0600

    Allow installing

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!python
+
+from setuptools import setup
+
+setup(
+    name='openakun',
+    version='0.0.1a',
+    description="Open real-time questing engine",
+    license='MIT',
+    packages=['openakun'],
+    install_requires=['Flask', 'passlib', 'fastpbkdf2', 'alembic',
+                      'SQLAlchemy', 'psycopg2', 'flask-login'],
+)
