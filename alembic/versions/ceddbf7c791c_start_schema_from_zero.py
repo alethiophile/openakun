63fc6da17b369bac746cbeb0a1cfe304a80c9a79
alethiophile <>
2017-09-24 16:45:46 -0600

    Remove obsolete migrations, start Alembic from blank DB again
    I have no idea why it want to detect this as a rename, but whatever.

--- alembic/versions/ceddbf7c791c_start_schema_from_zero.py
+++ alembic/versions/ceddbf7c791c_start_schema_from_zero.py
@@ -1,8 +1,8 @@
-"""Initial setup
+"""Start schema from zero
 
-Revision ID: 01dc05d8c691
+Revision ID: ceddbf7c791c
 Revises: 
-Create Date: 2017-09-18 22:35:49.036567
+Create Date: 2017-09-23 14:30:10.279128
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '01dc05d8c691'
+revision = 'ceddbf7c791c'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,27 +23,41 @@ def upgrade():
     sa.Column('email', sa.String(), nullable=True),
     sa.Column('password_hash', sa.String(), nullable=True),
     sa.Column('joined_date', sa.DateTime(), nullable=True),
-    sa.PrimaryKeyConstraint('id', name=op.f('pk_users'))
+    sa.PrimaryKeyConstraint('id', name=op.f('pk_users')),
+    sa.UniqueConstraint('name', name=op.f('uq_users_name'))
     )
     op.create_table('stories',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('title', sa.String(), nullable=True),
     sa.Column('description', sa.String(), nullable=True),
-    sa.Column('author_id', sa.Integer(), nullable=True),
+    sa.Column('author_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['author_id'], ['users.id'], name=op.f('fk_stories_author_id_users')),
     sa.PrimaryKeyConstraint('id', name=op.f('pk_stories'))
     )
     op.create_table('chapters',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('text', sa.String(), nullable=True),
-    sa.Column('posted_date', sa.DateTime(), nullable=True),
-    sa.Column('story_id', sa.Integer(), nullable=True),
+    sa.Column('title', sa.String(), nullable=True),
+    sa.Column('story_id', sa.Integer(), nullable=False),
+    sa.Column('is_appendix', sa.Boolean(), nullable=False),
+    sa.Column('order_idx', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['story_id'], ['stories.id'], name=op.f('fk_chapters_story_id_stories')),
     sa.PrimaryKeyConstraint('id', name=op.f('pk_chapters'))
+    )
+    op.create_table('posts',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('text', sa.String(), nullable=True),
+    sa.Column('posted_date', sa.DateTime(), nullable=False),
+    sa.Column('story_id', sa.Integer(), nullable=False),
+    sa.Column('chapter_id', sa.Integer(), nullable=False),
+    sa.Column('order_idx', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['chapter_id'], ['chapters.id'], name=op.f('fk_posts_chapter_id_chapters')),
+    sa.ForeignKeyConstraint(['story_id'], ['stories.id'], name=op.f('fk_posts_story_id_stories')),
+    sa.PrimaryKeyConstraint('id', name=op.f('pk_posts'))
     )
 
 
 def downgrade():
+    op.drop_table('posts')
     op.drop_table('chapters')
     op.drop_table('stories')
     op.drop_table('users')
