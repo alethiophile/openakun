ddb686e4b9c5feafea91010d6ab399b3237b4c9d
alethiophile <>
2017-09-24 21:36:08 -0600

    Added more tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,8 +11,12 @@ os.openakun_test_config = test_config
 
 from context import pages
 
-class LoginTest(unittest.TestCase):
-    def setUp(self):
+class OpenakunTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        # force new empty database
+        pages.db_engine = pages.models.create_engine("sqlite://")
+        pages.Session = pages.models.sessionmaker(bind=pages.db_engine)
         pages.app.testing = True
         self.client = pages.app.test_client()
         pages.init_db()
@@ -20,13 +24,38 @@ class LoginTest(unittest.TestCase):
         s.add(pages.create_user('admin', 'placeholder@example.com', 'test'))
         s.commit()
 
+    def login(self, user, password):
+        return self.client.post('/login',
+                                data={ 'user': user, 'pass': password },
+                                follow_redirects=True)
+
+    def logout(self):
+        return self.client.get('/logout', follow_redirects=True)
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+class LoginTest(OpenakunTestCase):
     def test_login(self):
         rv = self.client.get('/')
         self.assertIn(b'Log in', rv.data)
-        rv = self.client.post('/login',
-                              data={ 'user': 'admin', 'pass': 'test' },
-                              follow_redirects=True)
+        rv = self.login('admin', 'test')
         self.assertIn(b'Welcome, admin', rv.data)
 
-    def tearDown(self):
-        pass
+    def test_logout(self):
+        rv = self.logout()
+        self.assertIn(b'Log in', rv.data)
+
+class PostStoryTest(OpenakunTestCase):
+    def test_post(self):
+        rv = self.client.get('/new_story', follow_redirects=True)
+        self.assertIn(b'Please log in to access this page', rv.data)
+        self.login('admin', 'test')
+        rv = self.client.post('/new_story',
+                              data={ 'title': 'test title',
+                                     'description': 'test description' },
+                              follow_redirects=True)
+        self.assertIn(b'<h2>test title</h2>', rv.data)
+        self.assertRegex(rv.data.decode(),
+                         r'<div class="description">\s*test description\s*</div>')  # noqa: E501
