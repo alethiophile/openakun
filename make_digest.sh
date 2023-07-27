c53bd6315a8ffe833d6fe5f2fb22ff46fc41863f
alethiophile <mail@alethiophile.dev>
2023-07-26 21:27:33 -0600

    fixed digest script

--- make_digest.sh
+++ make_digest.sh
@@ -3,5 +3,5 @@
 # This script creates subresource integrity hashes for files, by
 # piping shasum output through base64.
 
-dgst=$(shasum -b -a 384 $1 | xxd -r -p | base64)
+dgst=$(sha384sum $1 | awk '{ print $1 }' | xxd -r -p | base64)
 echo "sha384-$dgst"
