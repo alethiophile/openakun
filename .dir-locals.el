7a6fe080f41bb3021e32602de80c9ec52a1e9bcc
alethiophile <mail@alethiophile.dev>
2023-11-08 13:47:12 -0700

    add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((js-mode . ((js-indent-level . 2)))
+ (web-mode . ((web-mode-markup-indent-offset . 4)
+              (web-mode-code-indent-offset . 4)
+              (web-mode-css-indent-offset . 4))))
